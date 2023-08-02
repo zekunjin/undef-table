5cc404ee15cb8aefa5bb5df82975e2eb9ab87313
zekunjin <zekun.jin@foxmail.com>
2023-08-02 17:48:34 +0800

    chore: add base layout

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+import { isNumber } from '../utils/common'
+
 interface TableColumn {
   title: string
   dataIndex: string
+  height: number | string
 }
 
 interface TableScroll {
-  x: string | number
-  y: string | number
+  x?: string | number
+  y?: string | number
 }
 
 interface TablePagination {
@@ -16,20 +19,43 @@ interface TablePagination {
 }
 
 interface Props {
-  columns: TableColumn[],
+  columns: TableColumn[]
+  dataSource: Record<string, any>[]
   scroll: TableScroll | undefined
   pagination: false | TablePagination | undefined
 }
 
-const props = withDefaults(defineProps<Props>(), {
-  columns: () => []
+withDefaults(defineProps<Props>(), {
+  columns: () => [],
+  dataSource: () => [],
+  scroll: () => ({})
 })
 </script>
 
 <template>
-  <div :style="{ display: 'grid', gridTemplateColumns: `repeat(${columns.length}, minmax(0, 1fr))` }">
-    <div v-for="col in props.columns" :key="col.dataIndex">
+  <div
+    :style="{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${columns.length}, minmax(0, 1fr))`,
+      height: isNumber(scroll?.y) ? `${scroll.y}px` : scroll?.y,
+      overflowX: scroll?.x ? 'auto' : 'hidden',
+      overflowY: scroll?.y ? 'auto' : 'hidden',
+    }"
+  >
+    <div v-for="col in columns" :key="col.dataIndex" :style="{ position: 'sticky', top: 0 }">
       {{ col.title }}
     </div>
+
+    <template v-for="row in dataSource" :key="row.key">
+      <div
+        v-for="({ dataIndex, height }) in columns"
+        :key="row[dataIndex]"
+        :style="{
+          height: isNumber(height) ? `${height}px` : height
+        }"
+      >
+        {{ row[dataIndex] }}
+      </div>
+    </template>
   </div>
 </template>
