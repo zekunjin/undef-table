6f21a2d39a1267aef46d3cf6255fa05f6014ae9a
zekunjin <zekun.jin@foxmail.com>
2023-08-03 11:26:57 +0800

    chore: use virtual list

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { isNumber } from '../utils/common'
+import { useVirtualList } from '@vueuse/core'
+import { calcCssUnit, isFunction } from '../utils/common'
 
 interface TableColumn {
   title: string
   dataIndex: string
-  height: number | string
 }
 
 interface TableScroll {
@@ -19,43 +19,54 @@ interface TablePagination {
 }
 
 interface Props {
+  rowKey: string
   columns: TableColumn[]
   dataSource: Record<string, any>[]
   scroll: TableScroll | undefined
   pagination: false | TablePagination | undefined
+  itemHeight: ((i: number) => number) | number
+  itemWidth: ((i: number) => number) | number
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
+  rowKey: 'key',
   columns: () => [],
   dataSource: () => [],
-  scroll: () => ({})
+  scroll: () => ({}),
+  itemHeight: 0
 })
+
+const { list, containerProps, wrapperProps } = useVirtualList(props.dataSource, {
+  itemHeight: props.itemHeight,
+  itemWidth: props.itemWidth,
+  overscan: 10
+})
+
+const calcItemHeight = (index: number) => {
+  if (isFunction(props.itemHeight)) { return props.itemHeight(index) }
+  return calcCssUnit(props.itemHeight)
+}
 </script>
 
 <template>
-  <div
-    :style="{
-      display: 'grid',
-      gridTemplateColumns: `repeat(${columns.length}, minmax(0, 1fr))`,
-      height: isNumber(scroll?.y) ? `${scroll.y}px` : scroll?.y,
-      overflowX: scroll?.x ? 'auto' : 'hidden',
-      overflowY: scroll?.y ? 'auto' : 'hidden',
-    }"
-  >
-    <div v-for="col in columns" :key="col.dataIndex" :style="{ position: 'sticky', top: 0 }">
-      {{ col.title }}
-    </div>
-
-    <template v-for="row in dataSource" :key="row.key">
+  <div v-bind="containerProps" :style="{ height: calcCssUnit(scroll?.y) }">
+    <div v-bind="wrapperProps">
       <div
-        v-for="({ dataIndex, height }) in columns"
-        :key="row[dataIndex]"
         :style="{
-          height: isNumber(height) ? `${height}px` : height
+          display: 'grid',
+          gridTemplateColumns: `repeat(${columns.length}, minmax(0, 1fr))`
         }"
       >
-        {{ row[dataIndex] }}
+        <div v-for="col in columns" :key="col.dataIndex" :style="{ position: 'sticky', top: 0 }">
+          {{ col.title }}
+        </div>
+
+        <template v-for="{ data, index } in list" :key="data.key">
+          <div v-for="({ dataIndex }) in columns" :key="data[dataIndex]" :style="{ height: calcItemHeight(index) }">
+            {{ data[dataIndex] }}
+          </div>
+        </template>
       </div>
-    </template>
+    </div>
   </div>
 </template>
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,1 +1,5 @@
 export const isNumber = (value: any): value is number => typeof value === 'number'
+
+export const isFunction = (value: any): value is ((...args: any[]) => any) => typeof value === 'function'
+
+export const calcCssUnit = (value?: string | number | undefined) => isNumber(value) ? `${value}px` : value
