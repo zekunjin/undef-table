f014bb2b88a9931cdd701e7051cf5902a70ff548
zekunjin <zekun.jin@foxmail.com>
2023-08-07 15:02:23 +0800

    chore: use vite dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [dts({ rollupTypes: true }), vue()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
